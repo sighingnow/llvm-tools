f24e3097f3e547a1a345f902eff13684d108041d
HE, Tao <sighingnow@gmail.com>
2019-04-08 13:43:28 +0800

    LLVM IR codegen.

--- kaleidoscope/app.cpp
+++ kaleidoscope/app.cpp
@@ -14,6 +14,19 @@
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 
+#include <llvm/ADT/APFloat.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Type.h>
+#include <llvm/IR/Value.h>
+#include <llvm/IR/Verifier.h>
+
 namespace qi = boost::spirit::qi;
 namespace phx = boost::phoenix;
 
@@ -68,9 +81,57 @@ using prototype_ptr_t = detail::ast_ptr_t<prototype_t>;
 using function_ptr_t = detail::ast_ptr_t<function_t>;
 using program_ptr_t = detail::ast_ptr_t<program_t>;
 
+struct scope_t {
+    scope_t() {
+        scopes.push_back(dict_t()); // top-level
+    }
+    void enter() {
+        scopes.push_back(dict_t());
+    }
+    void exit() {
+        if (scopes.empty()) {
+            throw std::runtime_error("Cannot exit at outside of top-level");
+        }
+        scopes.pop_back();
+    }
+    void witness(std::string const & name, llvm::Value *value) {
+        scopes.rbegin()->emplace(name, value); // FIXME: needs validation
+    }
+    llvm::Value *local(std::string const & name) {
+        if (scopes.rbegin()->find(name) == scopes.rbegin()->end()) {
+            return nullptr;
+        } else {
+            return scopes.rbegin()->at(name);
+        }
+    }
+    llvm::Value *global(std::string const & name) {
+        for (auto iter = scopes.rbegin(); iter != scopes.rend(); iter++) {
+            if (iter->find(name) != iter->end()) {
+                return iter->at(name);
+            }
+        }
+        return nullptr;
+    }
+
+    struct RTTI {
+        RTTI(scope_t & scope): scope(scope) {
+            scope.enter();
+        }
+        ~RTTI() {
+            scope.exit();
+        }
+      private:
+        scope_t &scope;
+    };
+private:
+    using dict_t = std::map<std::string, llvm::Value *>;
+    std::vector<dict_t> scopes;
+};
+
 struct expr_t {
     expr_t() = default;
     virtual std::string format() const = 0;
+    virtual llvm::Value *codegen(llvm::Module &, llvm::LLVMContext &, llvm::IRBuilder<> &, scope_t &) const = 0;
 };
 
 struct number_t: public expr_t {
@@ -78,28 +139,58 @@ struct number_t: public expr_t {
     std::string format() const override {
         return std::to_string(val);
     }
+    virtual llvm::Value *codegen(llvm::Module &, llvm::LLVMContext &, llvm::IRBuilder<> &, scope_t &) const override;
   private:
     double val;
 };
+
+llvm::Value *number_t::codegen(llvm::Module &, llvm::LLVMContext & Context, llvm::IRBuilder<> &, scope_t &) const {
+    return llvm::ConstantFP::get(Context, llvm::APFloat(val));
+}
 
 struct variable_t: public expr_t {
     variable_t(std::string const & name): name(name) {}
     std::string format() const override {
         return name;
     }
+    virtual llvm::Value *codegen(llvm::Module &, llvm::LLVMContext &, llvm::IRBuilder<> &, scope_t &) const override;
+    std::string const &getName() const noexcept {
+        return name;
+    }
   private:
     std::string name;
 };
+
+llvm::Value *variable_t::codegen(llvm::Module &, llvm::LLVMContext &, llvm::IRBuilder<> &, scope_t & Scope) const {
+    if (auto r = Scope.global(name)) {
+        return r;
+    } else {
+        throw std::runtime_error(fmt::format("Variable out of scope: {}", name));
+    }
+}
 
 struct unary_expr_t: public expr_t {
     unary_expr_t(char const & op, expr_ptr_t const & operand): op(op), operand(operand) {}
     std::string format() const override {
         return fmt::format("{} {}", op, operand->format());
     }
+    virtual llvm::Value *codegen(llvm::Module &, llvm::LLVMContext &, llvm::IRBuilder<> &, scope_t &) const override;
   private:
     char op;
     expr_ptr_t operand;
 };
+
+llvm::Value *unary_expr_t::codegen(llvm::Module & Module, llvm::LLVMContext & Context, llvm::IRBuilder<> & Builder, scope_t & Scope) const {
+    llvm::Value *v = operand->codegen(Module, Context, Builder, Scope);
+    switch (op) {
+        case '!':
+            return Builder.CreateNot(v);
+        case '-':
+            return Builder.CreateFNeg(v);
+        default:
+            throw std::runtime_error(fmt::format("Unknown unary operator: {}", op));
+    }
+}
 
 struct binary_expr_t: public expr_t {
     binary_expr_t(expr_ptr_t const & lhs, char const & op, expr_ptr_t const & rhs): lhs(lhs), op(op), rhs(rhs) {}
@@ -114,11 +205,35 @@ struct binary_expr_t: public expr_t {
     std::string format() const override {
         return fmt::format("({} {} {})", lhs->format(), op, rhs->format());
     }
+    virtual llvm::Value *codegen(llvm::Module &, llvm::LLVMContext &, llvm::IRBuilder<> &, scope_t &) const override;
   private:
     expr_ptr_t lhs;
     char op;
     expr_ptr_t rhs;
 };
+
+llvm::Value *binary_expr_t::codegen(llvm::Module & Module, llvm::LLVMContext & Context, llvm::IRBuilder<> & Builder, scope_t & Scope) const {
+    llvm::Value *l = lhs->codegen(Module, Context, Builder, Scope);
+    llvm::Value *r = rhs->codegen(Module, Context, Builder, Scope);
+    switch (op) {
+        case '+':
+            return Builder.CreateFAdd(l, r);
+        case '-':
+            return Builder.CreateFSub(l, r);
+        case '*':
+            return Builder.CreateFMul(l, r);
+        case '/':
+            return Builder.CreateFDiv(l, r);
+        case '<':
+            return Builder.CreateFCmpULT(l, r);
+        case '>':
+            return Builder.CreateFCmpUGT(l, r);
+        case '=':
+            return Builder.CreateFCmpUEQ(l, r);
+        default:
+            throw std::runtime_error(fmt::format("Unknown binary operator: {}", op));
+    }
+}
 
 struct call_t: public expr_t {
     call_t(std::string const & callee, std::vector<expr_ptr_t> const & args): callee(callee), args(args) {}
@@ -127,10 +242,20 @@ struct call_t: public expr_t {
         std::transform(args.begin(), args.end(), fmtargs.begin(), [](auto && arg) { return arg->format(); });
         return fmt::format("{}({})", callee, fmtargs);
     }
+    virtual llvm::Value *codegen(llvm::Module &, llvm::LLVMContext &, llvm::IRBuilder<> &, scope_t &) const override;
   private:
     std::string callee;
     std::vector<expr_ptr_t> args;
 };
+
+llvm::Value *call_t::codegen(llvm::Module & Module, llvm::LLVMContext & Context, llvm::IRBuilder<> & Builder, scope_t & Scope) const {
+    llvm::Function *F = Module.getFunction(callee);
+    std::vector<llvm::Value *> valargs(args.size());
+    std::transform(args.begin(), args.end(), valargs.begin(), [&](auto && expr) {
+        return expr->codegen(Module, Context, Builder, Scope);
+    });
+    return Builder.CreateCall(F, valargs);
+}
 
 struct branch_t: public expr_t {
     branch_t(expr_ptr_t const & condition, expr_ptr_t const & ifbody, expr_ptr_t const & elsebody):
@@ -138,9 +263,43 @@ struct branch_t: public expr_t {
     std::string format() const override {
         return fmt::format("(({}) ? ({}) : ({}))", condition->format(), ifbody->format(), elsebody->format());
     }
+    virtual llvm::Value *codegen(llvm::Module &, llvm::LLVMContext &, llvm::IRBuilder<> &, scope_t &) const override;
   private:
     expr_ptr_t condition, ifbody, elsebody;
 };
+
+llvm::Value *branch_t::codegen(llvm::Module & Module, llvm::LLVMContext & Context, llvm::IRBuilder<> & Builder, scope_t & Scope) const {
+    auto CondValue = condition->codegen(Module, Context, Builder, Scope);
+    fmt::print("insert block: {:p}\n", fmt::ptr(Builder.GetInsertBlock()));
+    auto F = Builder.GetInsertBlock()->getParent();
+
+    // LLVM IR requires all basic blocks to be “terminated” with a control flow
+    // instruction such as return or branch.
+
+    auto If = llvm::BasicBlock::Create(Context, "then");
+    auto Else = llvm::BasicBlock::Create(Context, "else");
+    auto Merge = llvm::BasicBlock::Create(Context, "merge");
+    Builder.CreateCondBr(CondValue, If, Else);
+
+    F->getBasicBlockList().push_back(If);
+    Builder.SetInsertPoint(If);
+    auto IfValue = ifbody->codegen(Module, Context, Builder, Scope);
+    Builder.CreateBr(Merge);
+    If = Builder.GetInsertBlock();
+
+    F->getBasicBlockList().push_back(Else);
+    Builder.SetInsertPoint(Else);
+    auto ElseValue = elsebody->codegen(Module, Context, Builder, Scope);
+    Builder.CreateBr(Merge);
+    Else = Builder.GetInsertBlock();
+
+    F->getBasicBlockList().push_back(Merge);
+    Builder.SetInsertPoint(Merge);
+    llvm::PHINode *Phi = Builder.CreatePHI(llvm::Type::getDoubleTy(Context), 2, "phi");
+    Phi->addIncoming(IfValue, If);
+    Phi->addIncoming(ElseValue, Else);
+    return Phi;
+}
 
 struct prototype_t {
     prototype_t(std::string const & name, std::vector<variable_ptr_t> const & args): name(name), args(args) {}
@@ -149,10 +308,28 @@ struct prototype_t {
         std::transform(args.begin(), args.end(), fmtargs.begin(), [](auto && arg) { return arg->format(); });
         return fmt::format("{}({});", name, fmtargs);
     }
+    llvm::Function *codegen(llvm::Module &, llvm::LLVMContext &, llvm::IRBuilder<> &, scope_t &) const;
+    std::string const &getName() const noexcept {
+        return name;
+    }
   private:
     std::string name;
     std::vector<variable_ptr_t> args;
 };
+
+llvm::Function *prototype_t::codegen(llvm::Module & Module, llvm::LLVMContext & Context, llvm::IRBuilder<> &, scope_t &) const {
+    std::vector<llvm::Type *> argtypes(args.size());
+    for (size_t i = 0; i < args.size(); ++i) {
+        argtypes[i] = llvm::Type::getDoubleTy(Context);
+    }
+    llvm::FunctionType *FType = llvm::FunctionType::get(llvm::Type::getDoubleTy(Context), argtypes, false);
+    llvm::Function *F = llvm::Function::Create(FType, llvm::Function::ExternalLinkage, name, Module);
+    size_t idx = 0;
+    for (auto && Arg: F->args()) {
+        Arg.setName(args[idx++]->getName());
+    }
+    return F;
+}
 
 struct function_t {
     function_t(std::string const & name, std::vector<variable_ptr_t> const & args, expr_ptr_t const & body):
@@ -161,10 +338,30 @@ struct function_t {
     std::string format() const {
         return fmt::format("{}\{\n{}\n\}", prototype->format(), body->format());
     }
+    llvm::Function *codegen(llvm::Module &, llvm::LLVMContext &, llvm::IRBuilder<> &, scope_t &) const;
   private:
     prototype_ptr_t prototype;
     expr_ptr_t body;
 };
+
+llvm::Function *function_t::codegen(llvm::Module & Module, llvm::LLVMContext & Context, llvm::IRBuilder<> & Builder, scope_t & Scope) const {
+    scope_t::RTTI rtti(Scope);
+    llvm::Function *F = prototype->codegen(Module, Context, Builder, Scope);
+    llvm::BasicBlock *BB = llvm::BasicBlock::Create(Context, prototype->getName(), F);
+    Builder.SetInsertPoint(BB);
+
+    for (auto && Arg: F->args()) {
+        Scope.witness(Arg.getName(), &Arg);
+    }
+    if (llvm::Value *Ret = body->codegen(Module, Context, Builder, Scope)) {
+        Builder.CreateRet(Ret);
+        llvm::verifyFunction(*F);
+        return F;
+    } else {
+        // F->eraseFromParent();
+        throw std::runtime_error(fmt::format("Failed codegen for function:\n{}", this->format()));
+    }
+}
 
 struct program_t {
     program_t(expr_ptr_t const & body): body(body) {}
@@ -179,11 +376,29 @@ struct program_t {
         std::transform(functions.begin(), functions.end(), fmtfuncs.begin(), [](auto && p) { return p->format(); });
         return fmt::format("Prototypes:\n{}\n\n Functions:\n{}\n\n Body:\n{}", fmtprotos, fmtfuncs, body->format());
     }
+    void codegen() const;
   private:
     std::vector<prototype_ptr_t> prototypes;
     std::vector<function_ptr_t> functions;
     expr_ptr_t body;
 };
+
+void program_t::codegen() const {
+    llvm::LLVMContext Context;
+    llvm::IRBuilder<> Builder(Context);
+    llvm::Module Module("kaleidoscope", Context);
+    scope_t Scope;
+    for (auto && p: prototypes) {
+        p->codegen(Module, Context, Builder, Scope);
+    }
+    for (auto && f: functions) {
+        f->codegen(Module, Context, Builder, Scope);
+    }
+    Module.dump();
+    // wrap body into an entry function.
+    auto entry = std::make_shared<function_t>("_main", std::vector<variable_ptr_t>{}, body);
+    entry->codegen(Module, Context, Builder, Scope)->dump();
+}
 
 class grammar_t: public qi::grammar<std::string::const_iterator, program_ptr_t(), qi::space_type> {
   public:
